69d1010d442a897013901eec9be21618ddb0f440
Nirvish Patel <nirvishpatel622@gmail.com>
2026-06-16 17:11:15 +0530

    Update one.cpp

--- Problems/one.cpp
+++ Problems/one.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+#include <map>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::map<int, int> repeat;
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++) {
+            int rem = target - nums[i];
+            if (repeat.count(rem)) {
+                ans.push_back(i);
+                ans.push_back(repeat[rem]);
+                return ans;
+            }
+            repeat[nums[i]] = i;
+        }
+        return ans;
+    }
+};
